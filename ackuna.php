cdc80fdbd781e576a9778dc2ac996db36251fc92
webdevtsu <webdevtsu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-07 16:56:13 +0000

    Better removal from excerpts. Trimmed code and added better comments.
    git-svn-id: https://plugins.svn.wordpress.org/ackuna-language-translation-plugin/trunk@406310 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ackuna.php
+++ ackuna.php
@@ -8,55 +8,60 @@ Author: Ackuna
 Author URI: http://www.ackuna.com/
 License: GPL2
 */
-/*  Copyright 2011 Ackuna (email : alex.buran@gmail.com)
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+/*
+Copyright 2011 Ackuna (email : alex.buran@gmail.com)
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
 class AckunaWidget {
-	//==========================================
-	// If your page is not written in English, replace your language below.
-	// You must use the language CODE of your website, not the full language name.
+	// ==========================================
+	// PLEASE NOTE:
+	// If your page is not written in English, you must update the line below.
+	// Use the language CODE of your website, not the full language name.
 	// For a full list of usable languages and codes, please visit: 
 	// http://code.google.com/apis/language/translate/v1/reference.html#LangNameArray
+	// 
 	// e.g. If your page is written in Spanish, then the line:
 	// var $ackuna_src = 'en';
 	// Should be changed to:
 	// var $ackuna_src = 'es';
-	
+	// ==========================================
 	var $ackuna_src = 'en';
-	//==========================================
 	
-	// Constructor
+	// Constructor.
 	function AckunaWidget (){
+		// Add functions to the content and excerpt.
 		add_filter('the_content', array(&$this, 'codeToContent'));
 		add_filter('get_the_excerpt', array(&$this, 'ackunaExcerptTrim'));
 	}
 	
+	// Called whenever content is shown.
 	function codeToContent ($content){  
-		// Add nothing to RSS feed.
+		// What we add depends on type.
 		if (is_feed()) {
+			// Add nothing to RSS feed.
 			return $content;
-		}
-		
-		// Add nothing to categories.
-		if (is_category()) {
+		} else if (is_category()) {
+			// Add nothing to categories.
 			return $content;
+		} else {
+			// For everything else we add the button to the content normally.
+			return $this->getAckunaCode() . $content;
 		}
-		
-		// Add the link to the content.
-		return $this->getAckunaCode() . $content;
 	}
 	
 	// Get the actual button code.
@@ -71,10 +76,8 @@ class AckunaWidget {
 		$ackuna_code	.= '<script type="text/javascript">';
 		$ackuna_code	.= sprintf('var ackuna_src = "%s";', $this->ackuna_src);
 		$ackuna_code	.= '</script>';
-		
 		// The following line may need to be removed if you already have jQuery on your site.
 		$ackuna_code	.= '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js" type="text/javascript"></script>';
-		
 		$ackuna_code	.= '<script src="http://s1.ackuna.com/_v_1/javascript/e3.js" type="text/javascript"></script>';
 		$ackuna_code	.= '</div>';
 		$ackuna_code	.= '</div>';
@@ -82,8 +85,11 @@ class AckunaWidget {
         return $ackuna_code;
 	}
 	
+	// Remove (what's left of) our button code from excerpts.
 	function ackunaExcerptTrim ($text) {
-		return str_replace('Translatorvar ackuna_src = "en";', '', $text);
+		$pattern		= '/Translatorvar ackuna_src = "(.*?)";/i';
+		$replacement	= '';
+		return preg_replace($pattern, $replacement, $text);
 	}
 }
 
